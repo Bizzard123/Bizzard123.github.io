4e8bb69b882b6ed328ed8fac33cb73feba10bee6
Arham <ar.hayat7@gmail.com>
2024-04-19 00:01:59 +0530

    Form

--- src/components/DarkSection.js
+++ src/components/DarkSection.js
@@ -1,8 +1,41 @@
-// DarkSection.jsx
-
 import React from 'react';
+import { useEffect, useState } from "react";
+    
 
 const DarkSection = () => {
+  const [isLoading, setIsLoading] = useState(false);
+
+    useEffect(() => {
+        const form = document.getElementById("my-form");
+    
+        const handleSubmit = (e) => {
+            e.preventDefault();
+            setIsLoading(true);
+            const data = new FormData(form);
+            const action = e.target.action;
+    
+            fetch(action, {
+                method: "POST",
+                body: data,
+            })
+                .then(() => {
+                    setIsLoading(false);
+                    alert("Form Submitted Successfully!");
+                    form.reset();
+                })
+                .catch((error) => {
+                    setIsLoading(false);
+                    alert(error);
+                });
+        };
+    
+        form.addEventListener("submit", handleSubmit);
+    
+        return () => {
+            form.removeEventListener("submit", handleSubmit);
+        };
+    }, []);
+
   return (
     <div className="DarkSection wow fadeInUp">
       <div className="left-content">
@@ -13,17 +46,19 @@ const DarkSection = () => {
 
       <div className="right-content">
         <h3>Get in Touch!</h3>
-        <form>
+        <form id='my-form' action='https://script.google.com/macros/s/AKfycbwj9YOQvBtbK-H_X6G1qVgWH-_ewnJxILKbflqzoDwEcQwHGVGAzj7_NnhPTO31xv3e/exec'>
           <div>
-            <input type="text" placeholder="Name" />
+            <input name='Name' required="required" type="text" placeholder="Name" />
           </div>
           <div>
-            <input type="email" placeholder="Email" />
+            <input type="email" name="Email" required="required" placeholder="Email" />
           </div>
           <div>
-            <input type="tel" placeholder="Number" />
+            <input type="tel" placeholder="Number" name="Number" required="required" />
           </div>
-          <button className='centered-button-inverted' type="submit">Submit</button>
+          <button className='centered-button-inverted' type="submit">
+          {isLoading ? "Please Wait..." : "Submit"}
+          </button>
         </form>
       </div>
     </div>
